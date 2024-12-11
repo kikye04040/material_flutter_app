e20e2b5d57fda0fcf8fe4d64954ebe3fdb037590
kikye04040 <kikye04040@gmail.com>
2024-12-12 00:40:45 +0900

    feat: 이미지 추가하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'imageWidget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MaterialFlutterApp(),
+      home: ImageWidgetApp(),
     );
   }
 }
