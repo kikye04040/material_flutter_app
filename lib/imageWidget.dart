26d95cf1e82ca69c299a84a1b0c598c4191bd9b4
kikye04040 <kikye04040@gmail.com>
2024-12-12 00:47:51 +0900

    feat: 폰트 적용

--- lib/imageWidget.dart
+++ lib/imageWidget.dart
@@ -17,7 +17,8 @@ class _ImageWidgetApp extends State<ImageWidgetApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Image.asset('image/flutter.png', width: 200, height: 100, fit: BoxFit.fill)
+              Image.asset('image/flutter.png', width: 200, height: 100, fit: BoxFit.fill),
+              Text('Hello Flutter', style: TextStyle(fontFamily: 'Pacifico', fontSize: 30, color: Colors.blue))
             ],
           ),
         ),
